af2860aad9448af37244bee077504d3ba210c318
Akash Kumar <princeakash259@gmail.com>
2025-09-25 09:15:38 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+ 
+--  Create Database and Schemas
+--  Script Purpose:
+--  This script creates a new database named 'DataWarehouse' after checking if it already exists.
+-- the script sets up three schemas within datbase : 'bronze', 'silver', 'gold'
+
+
+
+
+
+
+create database datawarehose;
+GO
+create schema bronze
+nGO
+create schema silver
+GO
+create schema gold
